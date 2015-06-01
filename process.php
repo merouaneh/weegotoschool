f076276bf63b51482a53aaf6dd7cadbeb74a8366
Akram Ben Aissi <akram.benaissi@gmail.com>
2015-06-01 12:54:29 +0200

    Background theme looks better

--- process.php
+++ process.php
@@ -1,13 +1,11 @@
 <?php
+include 'common.php';
+
 $json = json_encode($_POST);
 
 echo $json
 
-include 'common.php';
 $collection = get_collection(ROUTES_COLLECTION);
 $collection->insert($json);
-
-
-
 
 ?>

597b2cb5e45858af8186e6a377ab4d206a33dd8e
Akram Ben Aissi <akram.benaissi@gmail.com>
2015-06-01 13:00:15 +0200

    Background theme looks better

--- process.php
+++ process.php
@@ -6,4 +6,11 @@ echo $json;
 $collection = get_collection(ROUTES_COLLECTION);
 $collection->insert($json);
 
+echo $collection;
+echo "Collection inseree";
+$collection = get_collection(ROUTES_COLLECTION);
+echo $collection;
+
+
+
 ?>

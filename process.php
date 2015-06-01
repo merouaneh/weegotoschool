164c5fd15128c8904dd1b140a7358c39782d031c
Akram Ben Aissi <akram.benaissi@gmail.com>
2015-06-01 13:09:26 +0200

    Background theme looks better

--- process.php
+++ process.php
@@ -5,32 +5,3 @@ $json = json_encode($_POST);
 echo $json;
 ?>
 
-/*
-//  Check if we need to recreate the collection.
-$r_opts = array('r', 'recreate');
-if (true == is_option_set($r_opts) ) {
-   $collection = get_collection(ROUTES_COLLECTION);
-   $collection->drop();
-}
-//  Get tweets collection in MongoDB and last tweet ID.
-$collection = get_collection(ROUTES_COLLECTION);
-$lastobj    = $collection->findOne();
-$lastid     = $lastobj["id"];
-
-
-$collection = get_collection(ROUTES_COLLECTION);
-$collection->insert($json);
-
-echo $collection;
-?>
-<br>
-<br>
-<?php 
-echo "Collection inseree";
-$collection = get_collection(ROUTES_COLLECTION);
-echo $collection;
-
-
-
-?>
-*/
